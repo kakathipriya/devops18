8eb715aa7b8f9e1bebbdd69aab4dfb27b48277d1
kakathipriya <kakatihindupriya4kp@gmail.com>
2026-03-30 14:44:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-02dfbd4ff395f2a1b"
+    image_id           = "ami-073e5bc3ae6e46156"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t3.micro"
-    key_name = "march-kp"
+    key_name = "ganesh-kp"
     tags = {
         Name = "DevOps"
     }
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-05b5c04b3e4f5fc97", "subnet-078eaa4691fce41d5"]
+     subnets = ["subnet-0a7d0b753e7e6f0fa", "subnet-041810084f0a3a525"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
